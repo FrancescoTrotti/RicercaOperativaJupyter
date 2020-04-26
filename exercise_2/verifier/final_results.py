b849942c67ab57991fa329186c6c0f3f62a4d4cb
Ale <busatto.alessandro96@gmail.com>
2020-04-26 12:56:06 +0200

    Add final solution generator

--- exercise_2/verifier/final_results.py
+++ exercise_2/verifier/final_results.py
@@ -1,10 +1,33 @@
-def save_results():
-    file1 = open('../result/es1.txt', 'r')
-    p=file1.readlines()
+import fnmatch
+import os
 
-    for i in range(0, len(p)):
-        p[i] = list(map(int,p[i]))
-    print(str(p))
+def save_results():
+    path = '../result/'
+    n_file=len(fnmatch.filter(os.listdir(path), 'es*.txt'))
+    res = open(path + 'soluzione.txt', 'w')
+    for i in range(1,n_file+1):
+        file = open(path+'es'+str(i)+'.txt', 'r')
+        line=file.readlines()
+        if len(line)==1:
+            n=line[0].split(',')
+            res.write(str(i)+".\tSequenza: "+str(line[0])+"\t Lunghezza: "+str(len(n))+'\n')
+        else:
+            res.write(str(i)+".\tSequenze:\n")
+            for elem in line:
+                elem=elem[1:-2]
+                elem=elem.replace(" ", "")
+                aux=[]
+                num=elem.split(",")
+                for n in num:
+                    if int(n)!=0:
+                        aux.append(n)
+                res.write('\t\t'+str(aux)+'\n')
+            res.write('\tNumero di sottosequenze: '+str(len(line)))
+
+        file.close()
+
+
+    res.close()
 
 
 
